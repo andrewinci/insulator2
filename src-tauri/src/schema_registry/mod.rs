287a50db9d3a1d68773f2cd965c0fb6a95c3685a
andrewinci <andrewinci@gmail.com>
2022-09-25 07:15:43 +0100

    feat(BE): Get all schema versions for a given subject

--- src-tauri/src/schema_registry/mod.rs
+++ src-tauri/src/schema_registry/mod.rs
@@ -1,10 +1,9 @@
 use url::Url;
 use serde::{ de::DeserializeOwned, Deserialize, Serialize };
 
-use crate::{ configuration::model::SchemaRegistry, error::{ Result, TauriError } };
+use crate::{ configuration::model::SchemaRegistry, error::{ Result } };
 
 async fn get<T: DeserializeOwned>(url: String, config: &SchemaRegistry) -> Result<T> {
-    println!("{}", url);
     let client = reqwest::Client::new();
     let mut request = client.get(url);
     if let Some(username) = &config.username {
@@ -23,21 +22,18 @@ pub async fn list_subjects(config: SchemaRegistry) -> Result<Vec<String>> {
 }
 
 #[tauri::command]
-pub async fn get_schema(subject_name: String, config: SchemaRegistry) -> Result<Schema> {
+pub async fn get_schema(subject_name: String, config: SchemaRegistry) -> Result<Vec<Schema>> {
     let url = Url::parse(&config.endpoint)?.join(
         format!("/subjects/{}/versions/", subject_name).as_str()
     )?;
     let versions: Vec<i32> = get(url.to_string(), &config).await?;
-    if let Some(latest_version) = versions.iter().max() {
-        let latest_schema_url = url.join(&latest_version.to_string())?;
-        let latest_schema: Schema = get(latest_schema_url.to_string(), &config).await?;
-        Ok(latest_schema)
-    } else {
-        Err(TauriError {
-            error_type: "Schema registry".to_string(),
-            message: format!("No versions found for subject {:}", subject_name),
-        })
+    let mut schemas = Vec::<Schema>::new();
+    for v in versions {
+        let schema_url = url.join(&v.to_string())?;
+        let schema: Schema = get(schema_url.to_string(), &config).await?;
+        schemas.push(schema);
     }
+    Ok(schemas)
 }
 
 #[derive(Debug, Deserialize, Serialize)]
