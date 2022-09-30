6ed932545adfd1cd9b585c32dd8f1c4ede51e5ee
andrewinci <andrewinci@gmail.com>
2022-09-30 07:38:35 +0100

    feat(BE): Use lossy parsing for utf8 strings

--- src-tauri/src/kafka/consumer/parser.rs
+++ src-tauri/src/kafka/consumer/parser.rs
@@ -4,43 +4,30 @@ use crate::error::{ Result, TauriError };
 
 pub(super) fn parse_record(msg: OwnedMessage) -> Result<KafkaRecord> {
     Ok(KafkaRecord {
-        key: parse_string(msg.key()).map_err(|_| parse_error("Unable to map the key".into()))?,
-        value: parse_string(msg.payload()).map_err(|_| parse_error("Unable to map the value".into()))?,
+        key: parse_string(msg.key()),
+        value: parse_string(msg.payload()),
     })
 }
 
-fn parse_string(v: Option<&[u8]>) -> std::result::Result<Option<String>, String> {
-    match v {
-        Some(v) =>
-            String::from_utf8(Vec::from(v))
-                .map(Some)
-                .map_err(|_| "unable to parse the string to utf8".into()),
-        None => Ok(None),
-    }
-}
-
-fn parse_error(message: String) -> TauriError {
-    TauriError {
-        error_type: "Record parsing error".into(),
-        message,
-    }
+fn parse_string(v: Option<&[u8]>) -> Option<String> {
+    v.map(|v| String::from_utf8_lossy(v).into_owned())
 }
 
 #[test]
 fn parse_empty_array_to_string() {
     let vec = vec![];
     let res = parse_string(Some(&vec));
-    assert_eq!(res, Ok(Some("".into())))
+    assert_eq!(res, Some("".into()))
 }
 #[test]
 fn parse_none_to_string() {
     let res = parse_string(None);
-    assert_eq!(res, Ok(None))
+    assert_eq!(res, None)
 }
 
 #[test]
 fn parse_invalid_to_string() {
     let vec: Vec<u8> = vec![0x00, 0xff];
     let res = parse_string(Some(&vec));
-    assert_eq!(res, Err("unable to parse the string to utf8".into()))
+    assert_eq!(res.is_some(), true)
 }
