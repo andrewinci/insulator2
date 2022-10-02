2ac5ee81a125c6e9e2440484a726319315480b65
andrewinci <andrewinci@gmail.com>
2022-10-02 10:21:29 +0100

    feat(BE): add partitions to topic info

--- src-tauri/src/kafka/admin.rs
+++ src-tauri/src/kafka/admin.rs
@@ -1,5 +1,6 @@
 use std::time::Duration;
 use rdkafka::consumer::Consumer;
+use rdkafka::statistics::Partition;
 use serde::{ Serialize, Deserialize };
 
 use crate::configuration::{ Cluster };
@@ -8,8 +9,16 @@ use crate::error::Result;
 use super::consumer::create_consumer;
 
 #[derive(Serialize, Deserialize, Debug)]
+pub struct PartitionInfo {
+    id: i32,
+    isr: usize,
+    replicas: usize,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
 pub struct TopicInfo {
     name: String,
+    partitions: Vec<PartitionInfo>,
 }
 
 #[tauri::command]
@@ -18,7 +27,18 @@ pub async fn list_topics(cluster: Cluster) -> Result<Vec<TopicInfo>> {
         .fetch_metadata(None, Duration::from_secs(10))?
         .topics()
         .iter()
-        .map(|t| TopicInfo { name: t.name().to_string() })
+        .map(|t| TopicInfo {
+            name: t.name().to_string(),
+            partitions: t
+                .partitions()
+                .iter()
+                .map(|m| PartitionInfo {
+                    id: m.id(),
+                    isr: m.isr().len(),
+                    replicas: m.replicas().len(),
+                })
+                .collect(),
+        })
         .collect();
     Ok(topics)
 }
