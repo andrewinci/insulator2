1428c57985605b4d203d9153a41ed6ae2da839eb
andrewinci <andrewinci@gmail.com>
2022-10-09 20:30:38 +0100

    feat: cache schemas by subject name

--- src-tauri/src/lib/schema_registry/types.rs
+++ src-tauri/src/lib/schema_registry/types.rs
@@ -1,6 +1,6 @@
 use serde::{ Deserialize, Serialize };
 
-#[derive(Debug, Deserialize, Serialize)]
+#[derive(Debug, Deserialize, Serialize, Clone)]
 pub struct Schema {
     pub subject: String,
     pub id: i32,
