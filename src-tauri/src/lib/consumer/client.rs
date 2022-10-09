b37fb8acff2b5cea8bd8965d694f9316aeb02ce2
andrewinci <andrewinci@gmail.com>
2022-10-09 12:27:44 +0100

    refactor(BE): add interfaces in consumer

--- src-tauri/src/lib/consumer/client.rs
+++ src-tauri/src/lib/consumer/client.rs
@@ -1,7 +1,14 @@
 use async_trait::async_trait;
+use crate::lib::{ error::Result, types::RawKafkaRecord };
+use super::types::{ ConsumerOffsetConfiguration, ConsumerState };
 
 #[async_trait]
-pub trait Consumer {}
+pub trait Consumer {
+    async fn start(&self, topic: &str, offset_config: ConsumerOffsetConfiguration) -> Result<()>;
+    async fn stop(&self) -> Result<()>;
+    async fn get_record(&self, index: usize) -> Option<RawKafkaRecord>;
+    async fn get_consumer_state(&self) -> ConsumerState;
+}
 
 pub struct KafkaConsumer {}
 impl KafkaConsumer {
@@ -11,4 +18,17 @@ impl KafkaConsumer {
 }
 
 #[async_trait]
-impl Consumer for KafkaConsumer {}
+impl Consumer for KafkaConsumer {
+    async fn start(&self, topic: &str, offset_config: ConsumerOffsetConfiguration) -> Result<()> {
+        todo!()
+    }
+    async fn stop(&self) -> Result<()> {
+        todo!()
+    }
+    async fn get_record(&self, index: usize) -> Option<RawKafkaRecord> {
+        todo!()
+    }
+    async fn get_consumer_state(&self) -> ConsumerState {
+        todo!()
+    }
+}
