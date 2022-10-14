024180fe82fef4bdfd41cd825a15028826efc65c
andrewinci <andrewinci@gmail.com>
2022-10-14 08:20:31 +0100

    feat(FE): Show recent items in topics and schemas

--- src/pages/common/item-list.tsx
+++ src/pages/common/item-list.tsx
@@ -34,9 +34,9 @@ type ItemListProps = {
 // Common list page component
 export const ItemList = (props: ItemListProps) => {
   const { onItemSelected, onRefreshList, onAddClick, items, title, loading } = props;
-  const [state, setState] = useState<{ search: string; recent: Map<string, void> }>({
+  const [state, setState] = useState<{ search: string; recent: string[] }>({
     search: "",
-    recent: new Map<string, void>(),
+    recent: [],
   });
   const [windowSize, setWindowSize] = useState(getWindowSize());
 
@@ -47,8 +47,45 @@ export const ItemList = (props: ItemListProps) => {
   }, []);
 
   const filteredItems = useMemo(
-    () => items.filter((t) => t.toLowerCase().includes(state.search ?? "")).sort(),
-    [state.search, items]
+    () => ({
+      all: items.filter((t) => t.toLowerCase().includes(state.search ?? "")).sort(),
+      recent: state.recent.filter((t) => t.toLowerCase().includes(state.search ?? "")).sort(),
+      favorites: [],
+    }),
+    [items, state.recent, state.search]
+  );
+
+  const tabPanel = (title: string, panelItems: string[]) => (
+    <Tabs.Panel value={title} pt="xs">
+      {loading && (
+        <Center mt={10}>
+          <Loader />
+        </Center>
+      )}
+      {!loading && panelItems.length > 0 ? (
+        <FixedSizeList height={windowSize.innerHeight - 150} itemCount={panelItems.length} itemSize={38} width={"100%"}>
+          {({ index, style }) => (
+            <NavLink
+              onClick={() => {
+                const selectedItem = panelItems[index];
+                // avoid duplicates in the recent list
+                const newRecent = state.recent.filter((i) => i != selectedItem);
+                newRecent.push(selectedItem);
+                setState((s) => ({ ...s, recent: newRecent }));
+                onItemSelected(selectedItem);
+              }}
+              style={style}
+              noWrap
+              label={panelItems[index]}
+            />
+          )}
+        </FixedSizeList>
+      ) : (
+        <Center mt={20}>
+          <Text>Empty list</Text>
+        </Center>
+      )}
+    </Tabs.Panel>
   );
 
   return (
@@ -82,62 +119,13 @@ export const ItemList = (props: ItemListProps) => {
       <Divider mt={10} />
       <Tabs mt={10} variant="pills" defaultValue="all">
         <Tabs.List grow>
-          <Tabs.Tab
-            sx={{ height: 30 }}
-            rightSection={
-              <Badge
-                //sx={{ width: 16, height: 16, pointerEvents: 'none' }}
-                variant="filled"
-                size="md"
-                p={3}>
-                {filteredItems.length}/{props.items.length}
-              </Badge>
-            }
-            value="all"
-            icon={<IconList size={14} />}>
-            All
-          </Tabs.Tab>
-          <Tabs.Tab disabled sx={{ height: 30 }} value="recent" icon={<IconClock size={14} />}>
-            Recent
-          </Tabs.Tab>
-          <Tabs.Tab disabled sx={{ height: 30 }} value="favorites" icon={<IconStar size={14} />}>
-            Favorites
-          </Tabs.Tab>
+          <TabHeader title="All" icon="all" count={props.items.length} filtered={filteredItems.all.length} />
+          <TabHeader title="Recent" icon="recent" count={state.recent.length} filtered={filteredItems.recent.length} />
+          <TabHeader title="Favorites" icon="favorites" count={0} filtered={filteredItems.favorites.length} />
         </Tabs.List>
 
-        <Tabs.Panel value="all" pt="xs">
-          {/* todo: fix the below mess */}
-          {loading ? (
-            <Center mt={10}>
-              <Loader />
-            </Center>
-          ) : filteredItems.length > 0 ? (
-            <FixedSizeList
-              height={windowSize.innerHeight - 150}
-              itemCount={filteredItems.length}
-              itemSize={38}
-              width={"100%"}>
-              {({ index, style }) => (
-                <NavLink
-                  onClick={() => onItemSelected(filteredItems[index])}
-                  style={style}
-                  noWrap
-                  label={filteredItems[index]}
-                />
-              )}
-            </FixedSizeList>
-          ) : (
-            <Center mt={20}>
-              <Text>Empty list</Text>
-            </Center>
-          )}
-        </Tabs.Panel>
-
-        <Tabs.Panel value="recent" pt="xs">
-          <Center mt={20}>
-            <Text>Empty list</Text>
-          </Center>
-        </Tabs.Panel>
+        {tabPanel("all", filteredItems.all)}
+        {tabPanel("recent", filteredItems.recent)}
 
         <Tabs.Panel value="favorites" pt="xs">
           <Center mt={20}>
@@ -146,5 +134,32 @@ export const ItemList = (props: ItemListProps) => {
         </Tabs.Panel>
       </Tabs>
     </Container>
+  );
+};
+
+const TabHeader = (props: { title: string; count: number; filtered: number; icon: "all" | "recent" | "favorites" }) => {
+  const iconElement = useMemo(() => {
+    switch (props.icon) {
+      case "all":
+        return <IconList size={14} />;
+      case "recent":
+        return <IconClock size={14} />;
+      case "favorites":
+        return <IconStar size={14} />;
+    }
+  }, [props.icon]);
+
+  return (
+    <Tabs.Tab
+      sx={{ height: 30 }}
+      rightSection={
+        <Badge variant="filled" size="md" p={3}>
+          {props.filtered} / {props.count}
+        </Badge>
+      }
+      value={props.title.toLowerCase()}
+      icon={iconElement}>
+      {props.title}
+    </Tabs.Tab>
   );
 };
