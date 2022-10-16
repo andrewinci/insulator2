8f527711047ceb16fe501a3c6d1c56695ed93f8d
andrewinci <andrewinci@gmail.com>
2022-10-16 18:00:44 +0100

    build: update release config

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -37,5 +37,5 @@ update_json manifests/update-linux.json ".notes" "$RELEASE_NOTES"
 # Update windows manifest
 update_json manifests/update-windows.json '.version' "v$VERSION"
 update_json manifests/update-windows.json '.pub_date' "$NOW"
-update_json manifests/update-windows.json '.platforms."windows-x86_64".url' "https://github.com/andrewinci/insulator2/releases/download/v${VERSION}/Insulator 2_${VERSION}_x64_en-US.msi.zip"
+update_json manifests/update-windows.json '.platforms."windows-x86_64".url' "https://github.com/andrewinci/insulator2/releases/download/v${VERSION}/Insulator_2_${VERSION}_x64_en-US.msi.zip"
 update_json manifests/update-windows.json ".notes" "$RELEASE_NOTES"
