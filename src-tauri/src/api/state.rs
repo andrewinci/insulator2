406bc48df102969ba84edd4f6e1c1d9800c5fe12
andrewinci <andrewinci@gmail.com>
2022-10-18 19:32:53 +0100

    refactor(BE): extract build_cluster fn

--- src-tauri/src/api/state.rs
+++ src-tauri/src/api/state.rs
@@ -2,6 +2,7 @@ use std::{collections::HashMap, sync::Arc};
 
 use futures::lock::Mutex;
 use log::debug;
+use tauri::App;
 
 use crate::lib::{Cluster, ConfigStore};
 
@@ -18,19 +19,24 @@ impl AppState {
         let mut map = clusters.lock().await;
         if map.get(cluster_id).is_none() {
             debug!("Init cluster {}", cluster_id);
-            let configurations = ConfigStore::new()
-                .get_configuration()
-                .expect("Unable to get the configuration");
-            let cluster_config = configurations
-                .clusters
-                .iter()
-                .find(|c| c.id == cluster_id)
-                .expect("Unable to find the cluster config");
-            let cluster = Cluster::new(cluster_config);
+            let cluster = AppState::build_new_cluster(cluster_id);
             map.insert(cluster_id.into(), cluster);
         }
         map.get(cluster_id)
             .expect("Something went wrong retrieving a cluster that must be in the clusters vector")
-            .clone()
+            .clone() //todo: return a reference
+    }
+
+    fn build_new_cluster<'a>(cluster_id: &str) -> Cluster {
+        debug!("Init cluster {}", cluster_id);
+        let configurations = ConfigStore::new()
+            .get_configuration()
+            .expect("Unable to get the configuration");
+        let cluster_config = configurations
+            .clusters
+            .iter()
+            .find(|c| c.id == cluster_id)
+            .expect("Unable to find the cluster config");
+        Cluster::new(cluster_config)
     }
 }
