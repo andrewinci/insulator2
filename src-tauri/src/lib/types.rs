c7f67d938884a61a00eef5c36bf2273a3f74a905
andrewinci <andrewinci@gmail.com>
2022-11-02 03:11:45 +0000

    bump: rdkafka

--- src-tauri/src/lib/types.rs
+++ src-tauri/src/lib/types.rs
@@ -1,6 +1,6 @@
 use serde::{Deserialize, Serialize};
 
-#[derive(Debug, Serialize, Deserialize, Clone, PartialEq)]
+#[derive(Debug, Serialize, Deserialize, Clone, Eq, PartialEq)]
 pub struct KafkaRecord<T> {
     pub payload: Option<T>,
     pub key: Option<T>,
