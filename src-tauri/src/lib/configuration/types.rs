b16f7236d22fc78b5a9513c28d28cb2d10e27ca1
andrewinci <andrewinci@gmail.com>
2022-11-08 20:23:50 +0000

    chore: add tests to config store

--- src-tauri/src/lib/configuration/types.rs
+++ src-tauri/src/lib/configuration/types.rs
@@ -1,6 +1,6 @@
 use serde::{Deserialize, Serialize};
 
-#[derive(Serialize, Deserialize, Debug, Default)]
+#[derive(Serialize, Deserialize, Debug, Default, PartialEq, Eq)]
 pub struct InsulatorConfig {
     pub clusters: Vec<ClusterConfig>,
     pub theme: Option<Theme>,
@@ -10,13 +10,13 @@ pub struct InsulatorConfig {
     pub use_regex: Option<bool>,
 }
 
-#[derive(Serialize, Deserialize, Debug)]
+#[derive(Serialize, Deserialize, Debug, PartialEq, Eq)]
 pub enum Theme {
     Dark,
     Light,
 }
 
-#[derive(Serialize, Deserialize, Debug, Clone, Default)]
+#[derive(Serialize, Deserialize, Debug, Clone, Default, PartialEq, Eq)]
 pub struct ClusterConfig {
     pub id: String,
     pub name: String,
@@ -26,7 +26,7 @@ pub struct ClusterConfig {
     pub schema_registry: Option<SchemaRegistryConfig>,
 }
 
-#[derive(Serialize, Deserialize, Debug, Clone, Default)]
+#[derive(Serialize, Deserialize, Debug, Clone, Default, PartialEq, Eq)]
 pub enum AuthenticationConfig {
     Ssl {
         ca: String,
@@ -44,7 +44,7 @@ pub enum AuthenticationConfig {
     None,
 }
 
-#[derive(Serialize, Deserialize, Debug, Clone)]
+#[derive(Serialize, Deserialize, Debug, Clone, PartialEq, Eq)]
 pub struct SchemaRegistryConfig {
     pub endpoint: String,
     pub username: Option<String>,
