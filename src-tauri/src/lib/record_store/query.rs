a429d0f9b7e7eb9eeb29f1e3f09da70b0adc8b73
andrewinci <andrewinci@gmail.com>
2022-12-03 07:00:17 +0000

    refactor sqlite store

---
+++ src-tauri/src/lib/record_store/query.rs
@@ -0,0 +1,21 @@
+pub struct Query {
+    pub cluster_id: String,
+    pub topic_name: String,
+    pub offset: i64,
+    pub limit: i64,
+    pub query_template: String,
+}
+impl Query {
+    pub const SELECT_WITH_OFFSET_LIMIT_QUERY : &str = "SELECT partition, offset, timestamp, key, payload FROM {:topic} ORDER BY timestamp desc LIMIT {:limit} OFFSET {:offset}";
+
+    #[cfg(test)]
+    pub fn select_any(cluster_id: &str, topic_name: &str, offset: i64, limit: i64) -> Query {
+        Query {
+            cluster_id: cluster_id.into(),
+            topic_name: topic_name.into(),
+            limit,
+            offset,
+            query_template: Query::SELECT_WITH_OFFSET_LIMIT_QUERY.into(),
+        }
+    }
+}
