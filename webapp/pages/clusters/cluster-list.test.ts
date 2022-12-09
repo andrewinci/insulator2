3dd6f42bb5dcfb04fb9a1062e54a542ed639c185
Andrea Vinci <andrea.vinci@kaluza.com>
2022-12-09 18:00:00 +0000

    feat: improve cluster search

---
+++ webapp/pages/clusters/cluster-list.test.ts
@@ -0,0 +1,19 @@
+import { it, describe, expect } from "vitest";
+import { containsAllWords } from "./cluster-list";
+describe("containsAllWords", () => {
+  it("should return false if no word is contained in the text", () => {
+    expect(containsAllWords("a b c", "d e f") == false);
+    expect(containsAllWords("ab cd-ed", "cd-ef") == false);
+    expect(containsAllWords("", "a") == false);
+  });
+  it("should return true if all searched words are contained in the text", () => {
+    expect(containsAllWords("a b c", "a"));
+    expect(containsAllWords("ab cd-ed", "a"));
+    expect(containsAllWords("ab cd-ed", "ab cd ed"));
+    expect(containsAllWords("ab cd - ed", "ab cd ed"));
+  });
+  it("should ignore casing", () => {
+    expect(containsAllWords("a b c", "A B C"));
+    expect(containsAllWords("ab cd-ed", "AC") == false);
+  });
+});
