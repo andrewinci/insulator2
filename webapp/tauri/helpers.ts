d8bf3a35c3ff328d1e015be5acadbad1bd600394
andrewinci <andrewinci@gmail.com>
2022-12-14 08:39:36 +0000

    map JKSs to SSL

--- webapp/tauri/helpers.ts
+++ webapp/tauri/helpers.ts
@@ -1,5 +1,8 @@
 import { useQuery } from "@tanstack/react-query";
+import { invoke } from "@tauri-apps/api";
 import { platform } from "@tauri-apps/api/os";
+import { addNotification } from "providers";
+import { format, TauriError } from "./error";
 
 type Platform = "linux" | "darwin" | "win";
 
@@ -19,4 +22,23 @@ export const usePlatform = (): Platform | undefined => {
     })
   );
   return data;
+};
+
+export const parseTruststore = async (location: string, password: string | undefined): Promise<string> => {
+  return await invoke<string>("parse_truststore", { location, password }).catch((err: TauriError) => {
+    addNotification({ type: "error", title: "Parse truststore failed", description: format(err) });
+    return Promise.reject(err);
+  });
+};
+
+type UserCertificate = {
+  certificate: string;
+  key: string;
+};
+
+export const parseKeystore = async (location: string, password: string | undefined): Promise<UserCertificate> => {
+  return await invoke<UserCertificate>("parse_keystore", { location, password }).catch((err: TauriError) => {
+    addNotification({ type: "error", title: "Parse keystore failed", description: format(err) });
+    return Promise.reject(err);
+  });
 };
