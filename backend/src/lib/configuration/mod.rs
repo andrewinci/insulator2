87c7d5eb40f3f8e5dfba1711a42a3d6b49b1d2a5
andrewinci <andrewinci@gmail.com>
2022-12-23 09:48:21 +0000

    chore: use a single conf provider for the entire app lifecycle
    commit c5f6bc131c596d946363fded9a4c40a515870154
    Author: andrewinci <andrewinci@gmail.com>
    Date:   Fri Dec 23 09:18:12 2022 +0000
    
        reuse the same configuration provider for the entire app lifecycle
    
    commit 0b5c5f917b0c7ac2ef3e90226035aa70a9bff9bb
    Author: andrewinci <andrewinci@gmail.com>
    Date:   Fri Dec 23 09:07:53 2022 +0000
    
        rename config provider

--- backend/src/lib/configuration/mod.rs
+++ backend/src/lib/configuration/mod.rs
@@ -1,9 +1,9 @@
-mod config_store;
+mod configuration_provider;
 mod kafka_client_config;
 mod legacy_config;
 mod store_types;
 mod types;
 
-pub use config_store::ConfigStore;
+pub use configuration_provider::ConfigurationProvider;
 pub use kafka_client_config::build_kafka_client_config;
 pub use types::*;
