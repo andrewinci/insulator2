52456f689cc8d199b232f1d7cde41871e3b21aef
andrewinci <andrewinci@gmail.com>
2022-12-27 22:10:26 +0000

    Fix test

--- backend/src/integration_tests/admin_it.rs
+++ backend/src/integration_tests/admin_it.rs
@@ -6,63 +6,52 @@ mod integration_tests {
         consumer::{stream_consumer::StreamConsumer, Consumer},
     };
     use std::time::Duration;
-    use testcontainers::{clients, images::kafka};
+    use testcontainers::{
+        clients::{self, Cli},
+        images::kafka::{self, Kafka},
+        Container,
+    };
 
     use crate::lib::{
         admin::{KafkaAdmin, TopicAdmin},
         configuration::{ClusterConfig, Favorites},
+        Cluster,
     };
 
-    struct KafkaTestFixture {
-        pub cluster_config: ClusterConfig,
-        pub consumer: StreamConsumer,
-        // pub producer: FutureProducer,
-        // pub admin: AdminClient<DefaultClientContext>,
-        pub tmo: Duration,
+    fn build_kafka_client<T: FromClientConfig>(bootstrap_servers: &str) -> T {
+        ClientConfig::new()
+            .set("group.id", "testcontainer-rs")
+            .set("bootstrap.servers", bootstrap_servers)
+            .set("session.timeout.ms", "6000")
+            .set("enable.auto.commit", "false")
+            .set("auto.offset.reset", "earliest")
+            .create()
+            .expect("Unable to create the kafka client")
     }
 
-    fn init_test() -> KafkaTestFixture {
-        let docker = clients::Cli::default();
-        let kafka_node = docker.run(kafka::Kafka::default());
-
-        let bootstrap_servers = format!("127.0.0.1:{}", kafka_node.get_host_port_ipv4(kafka::KAFKA_PORT));
-
-        fn build_kafka_client<T: FromClientConfig>(bootstrap_servers: &str) -> T {
-            ClientConfig::new()
-                .set("group.id", "testcontainer-rs")
-                .set("bootstrap.servers", bootstrap_servers)
-                .set("session.timeout.ms", "6000")
-                .set("enable.auto.commit", "false")
-                .set("auto.offset.reset", "earliest")
-                .create()
-                .expect("Unable to create the kafka client")
-        }
-
-        KafkaTestFixture {
-            tmo: Duration::from_secs(30),
-            consumer: build_kafka_client(&bootstrap_servers),
-            // producer: build_kafka_client(&bootstrap_servers),
-            // admin: build_kafka_client(&bootstrap_servers),
-            cluster_config: ClusterConfig {
-                id: "cluster-id".into(),
-                name: "test-cluster-name".into(),
-                endpoint: bootstrap_servers,
-                authentication: crate::lib::configuration::AuthenticationConfig::None,
-                schema_registry: None,
-                favorites: Favorites::default(),
-            },
+    fn build_cluster_config(bootstrap_servers: String) -> ClusterConfig {
+        ClusterConfig {
+            id: "cluster-id".into(),
+            name: "test-cluster-name".into(),
+            endpoint: bootstrap_servers,
+            authentication: crate::lib::configuration::AuthenticationConfig::None,
+            schema_registry: None,
+            favorites: Favorites::default(),
         }
     }
 
     #[tokio::test]
     async fn produce_and_consume_messages() {
         // arrange
-        let KafkaTestFixture {
-            tmo,
-            cluster_config,
-            consumer,
-            ..
-        } = init_test();
+        let tmo = Duration::from_secs(30);
+        let docker = clients::Cli::default();
+        let kafka_node = docker.run(kafka::Kafka::default());
+
+        let bootstrap_servers = format!("127.0.0.1:{}", kafka_node.get_host_port_ipv4(kafka::KAFKA_PORT));
+        kafka_node.start();
+
+        let consumer: StreamConsumer = build_kafka_client(&bootstrap_servers.as_str());
+        let cluster_config = build_cluster_config(bootstrap_servers);
 
         let sut = KafkaAdmin::new(&cluster_config, tmo.clone()).expect("Unable to create the admin client");
         let test_topic_name = "test_topic_name";
