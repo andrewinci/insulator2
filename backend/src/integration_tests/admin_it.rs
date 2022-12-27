93c7c802c65616e0a38ae759b50562c909d2bfc8
andrewinci <andrewinci@gmail.com>
2022-12-27 11:39:13 +0000

    test(integration): create a topic

--- backend/src/integration_tests/admin_it.rs
+++ backend/src/integration_tests/admin_it.rs
@@ -1,68 +1,90 @@
 #[cfg(target_os = "linux")]
 #[cfg(test)]
 mod integration_tests {
-    use futures::StreamExt;
     use rdkafka::{
-        config::ClientConfig,
+        config::{ClientConfig, FromClientConfig},
         consumer::{stream_consumer::StreamConsumer, Consumer},
-        producer::{FutureProducer, FutureRecord},
-        Message,
     };
     use std::time::Duration;
     use testcontainers::{clients, images::kafka};
 
-    #[tokio::test]
-    async fn produce_and_consume_messages() {
+    use crate::lib::{
+        admin::{KafkaAdmin, TopicAdmin},
+        configuration::{ClusterConfig, Favorites},
+    };
+
+    struct KafkaTestFixture {
+        pub cluster_config: ClusterConfig,
+        pub consumer: StreamConsumer,
+        // pub producer: FutureProducer,
+        // pub admin: AdminClient<DefaultClientContext>,
+        pub tmo: Duration,
+    }
+
+    fn init_test() -> KafkaTestFixture {
         let docker = clients::Cli::default();
         let kafka_node = docker.run(kafka::Kafka::default());
 
         let bootstrap_servers = format!("127.0.0.1:{}", kafka_node.get_host_port_ipv4(kafka::KAFKA_PORT));
 
-        let producer = ClientConfig::new()
-            .set("bootstrap.servers", &bootstrap_servers)
-            .set("message.timeout.ms", "5000")
-            .create::<FutureProducer>()
-            .expect("Failed to create Kafka FutureProducer");
-
-        let consumer = ClientConfig::new()
-            .set("group.id", "testcontainer-rs")
-            .set("bootstrap.servers", &bootstrap_servers)
-            .set("session.timeout.ms", "6000")
-            .set("enable.auto.commit", "false")
-            .set("auto.offset.reset", "earliest")
-            .create::<StreamConsumer>()
-            .expect("Failed to create Kafka StreamConsumer");
-
-        let topic = "test-topic";
-
-        let number_of_messages_to_produce = 5_usize;
-        let expected: Vec<String> = (0..number_of_messages_to_produce)
-            .map(|i| format!("Message {}", i))
-            .collect();
-
-        for (i, message) in expected.iter().enumerate() {
-            producer
-                .send(
-                    FutureRecord::to(topic).payload(message).key(&format!("Key {}", i)),
-                    Duration::from_secs(0),
-                )
-                .await
-                .unwrap();
+        fn build_kafka_client<T: FromClientConfig>(bootstrap_servers: &str) -> T {
+            ClientConfig::new()
+                .set("group.id", "testcontainer-rs")
+                .set("bootstrap.servers", bootstrap_servers)
+                .set("session.timeout.ms", "6000")
+                .set("enable.auto.commit", "false")
+                .set("auto.offset.reset", "earliest")
+                .create()
+                .expect("Unable to create the kafka client")
         }
 
-        consumer.subscribe(&[topic]).expect("Failed to subscribe to a topic");
+        KafkaTestFixture {
+            tmo: Duration::from_secs(30),
+            consumer: build_kafka_client(&bootstrap_servers),
+            // producer: build_kafka_client(&bootstrap_servers),
+            // admin: build_kafka_client(&bootstrap_servers),
+            cluster_config: ClusterConfig {
+                id: "cluster-id".into(),
+                name: "test-cluster-name".into(),
+                endpoint: bootstrap_servers,
+                authentication: crate::lib::configuration::AuthenticationConfig::None,
+                schema_registry: None,
+                favorites: Favorites::default(),
+            },
+        }
+    }
 
-        let mut message_stream = consumer.stream();
-        for produced in expected {
-            let borrowed_message = tokio::time::timeout(Duration::from_secs(10), message_stream.next())
+    #[tokio::test]
+    async fn produce_and_consume_messages() {
+        // arrange
+        let KafkaTestFixture {
+            tmo,
+            cluster_config,
+            consumer,
+            ..
+        } = init_test();
+
+        let sut = KafkaAdmin::new(&cluster_config, tmo.clone()).expect("Unable to create the admin client");
+        let test_topic_name = "test_topic_name";
+        // test create a topic
+        {
+            let partition_count = 7_usize;
+            // act
+            sut.create_topic(test_topic_name, partition_count as i32, 1, false)
                 .await
-                .unwrap()
-                .unwrap();
+                .expect("Unable to create the test topic");
 
-            assert_eq!(
-                produced,
-                borrowed_message.unwrap().payload_view::<str>().unwrap().unwrap()
-            );
+            // assert
+            let metadata = consumer
+                .fetch_metadata(None, tmo)
+                .expect("Unable to retrieve the metadata");
+            let tp: Vec<_> = metadata
+                .topics()
+                .iter()
+                .map(|t| (t.name(), t.partitions().len()))
+                .collect();
+
+            assert!(tp.contains(&(test_topic_name, partition_count)))
         }
     }
 }
