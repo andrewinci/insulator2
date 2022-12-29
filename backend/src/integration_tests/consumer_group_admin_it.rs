4ec874ce5cb4a49744dad5c036c60c9d5e5205b9
andrewinci <andrewinci@gmail.com>
2022-12-29 10:54:30 +0000

    add consumer group it

---
+++ backend/src/integration_tests/consumer_group_admin_it.rs
@@ -0,0 +1,113 @@
+#![cfg(test)]
+use rdkafka::{
+    consumer::{BaseConsumer, Consumer},
+    producer::{BaseProducer, BaseRecord},
+};
+use std::time::Duration;
+
+use crate::{
+    integration_tests::KafkaTest,
+    lib::admin::{ConsumerGroupAdmin, KafkaAdmin},
+};
+
+#[tokio::test]
+async fn test_consumer_groups_admin_client() {
+    // arrange
+    let test_fixture = KafkaTest::new();
+
+    let consumer: BaseConsumer = test_fixture.build_kafka_client();
+    let producer: BaseProducer = test_fixture.build_kafka_client();
+    let cluster_config = test_fixture.build_cluster_config();
+
+    let sut = KafkaAdmin::new(&cluster_config, test_fixture.tmo).expect("Unable to create the admin client");
+    let test_topic_name = "test_topic_name";
+    // produce a record to create the topic
+    producer
+        .send(BaseRecord::to(&test_topic_name).key("key").payload("content"))
+        .expect("Unable to produce the message");
+
+    // test list consumer groups
+    {
+        // arrange
+        // create a consumer group by consuming the test topic
+        consumer
+            .subscribe(&[&test_topic_name])
+            .expect("Unable to subscribe to the test topic");
+        let consumed = consumer.poll(Duration::from_secs(60)).unwrap();
+        assert!(consumed.is_ok());
+        // act
+        let res = sut.list_consumer_groups().expect("");
+        // assert
+        assert_eq!(res, vec![test_fixture.default_consumer_group])
+    }
+
+    // test create consumer groups
+    {
+        let test_consumer_group = "test-consumer-group";
+        // act
+        let res = sut
+            .set_consumer_group(
+                test_consumer_group,
+                &[&test_topic_name],
+                &crate::lib::consumer::types::ConsumerSessionConfiguration::Beginning,
+            )
+            .await;
+        // assert
+        assert!(res.is_ok());
+        let consumer_groups = consumer
+            .fetch_group_list(None, test_fixture.tmo)
+            .expect("Unable to retrieve the list of consumer groups");
+        assert!(!consumer_groups
+            .groups()
+            .iter()
+            .filter(|cg| cg.name() == test_consumer_group)
+            .collect::<Vec<_>>()
+            .is_empty())
+    }
+
+    // test delete consumer group
+    {
+        let test_consumer_group = "test-delete-consumer-group";
+        sut.set_consumer_group(
+            test_consumer_group,
+            &[&test_topic_name],
+            &crate::lib::consumer::types::ConsumerSessionConfiguration::Beginning,
+        )
+        .await
+        .expect("Unable to create the consumer group for the test");
+        // act
+        let res = sut.delete_consumer_group(test_consumer_group).await;
+        // assert
+        assert!(res.is_ok());
+        let consumer_groups = consumer
+            .fetch_group_list(None, test_fixture.tmo)
+            .expect("Unable to retrieve the list of consumer groups");
+        assert!(consumer_groups
+            .groups()
+            .iter()
+            .filter(|cg| cg.name() == test_consumer_group)
+            .collect::<Vec<_>>()
+            .is_empty())
+    }
+
+    // test describe consumer group
+    {
+        let test_consumer_group = "test-delete-consumer-group";
+        sut.set_consumer_group(
+            test_consumer_group,
+            &[&test_topic_name],
+            &crate::lib::consumer::types::ConsumerSessionConfiguration::Beginning,
+        )
+        .await
+        .expect("Unable to create the consumer group for the test");
+        // act
+        let res = sut
+            .describe_consumer_group(test_consumer_group, true)
+            .await
+            .expect("Unable to describe the consumer group");
+        // assert
+        assert_eq!(res.offsets.len(), 1);
+        assert_eq!(res.offsets[0].offset, 0);
+        assert_eq!(res.offsets[0].topic, test_topic_name);
+    }
+}
---
+++ backend/src/integration_tests/topic_admin_it.rs
@@ -0,0 +1,148 @@
+#![cfg(test)]
+
+use rdkafka::{
+    consumer::{stream_consumer::StreamConsumer, Consumer},
+    producer::{FutureProducer, FutureRecord},
+};
+use std::{collections::HashSet, time::Duration};
+
+use crate::{
+    integration_tests::KafkaTest,
+    lib::admin::{KafkaAdmin, TopicAdmin},
+};
+
+#[tokio::test]
+async fn test_topic_admin_client() {
+    // arrange
+    let test_fixture = KafkaTest::new();
+
+    let consumer: StreamConsumer = test_fixture.build_kafka_client();
+    let cluster_config = test_fixture.build_cluster_config();
+
+    let sut = KafkaAdmin::new(&cluster_config, test_fixture.tmo).expect("Unable to create the admin client");
+
+    // test create a topic
+    {
+        let test_topic_name = "test_topic_name";
+        let partition_count = 7_usize;
+        // act
+        sut.create_topic(test_topic_name, partition_count as i32, 1, false)
+            .await
+            .expect("Unable to create the test topic");
+
+        // assert
+        let metadata = consumer
+            .fetch_metadata(None, test_fixture.tmo)
+            .expect("Unable to retrieve the metadata");
+        let tp: Vec<_> = metadata
+            .topics()
+            .iter()
+            .map(|t| (t.name(), t.partitions().len()))
+            .collect();
+
+        assert!(tp.contains(&(test_topic_name, partition_count)))
+    }
+
+    // test retrieve the list of topics
+    {
+        sut.create_topic("another_test_topic", 1, 1, true)
+            .await
+            .expect("Unable to create a test topic");
+        // act
+        let res = sut.list_topics().await.expect("Unable to retrieve the list of topics");
+        // assert
+        let topic_set: HashSet<_> = res.iter().map(|t| (t.name.clone(), t.partitions.len())).collect();
+        let metadata = consumer
+            .fetch_metadata(None, test_fixture.tmo)
+            .expect("Unable to retrieve the metadata");
+
+        let expected_topics: HashSet<_> = metadata
+            .topics()
+            .iter()
+            .map(|t| (t.name().to_string(), t.partitions().len()))
+            .collect();
+
+        assert_eq!(topic_set, expected_topics)
+    }
+
+    // test get topic
+    {
+        // arrange
+        let test_topic_name = "test_get_topic";
+        let partition_count = 3_usize;
+        sut.create_topic(test_topic_name, partition_count as i32, 1, true)
+            .await
+            .expect("Unable to create a test topic");
+        // act
+        let res = sut.get_topic(test_topic_name).expect("Unable to get topic");
+        // assert
+        assert_eq!(res.name, test_topic_name.to_string());
+        assert_eq!(res.partitions.len(), partition_count);
+    }
+
+    // test get topic info
+    {
+        // arrange
+        let test_topic_name = "test_get_topic_info";
+        let partition_count = 3_usize;
+        sut.create_topic(test_topic_name, partition_count as i32, 1, true)
+            .await
+            .expect("Unable to create a test topic");
+        // act
+        let res = sut.get_topic_info(test_topic_name).await.expect("get_topic_info error");
+        // assert
+        assert_eq!(res.name, test_topic_name.to_string());
+        assert_eq!(res.partitions.len(), partition_count);
+        assert_eq!(
+            res.configurations.get("cleanup.policy").unwrap(),
+            &Some("compact".to_string())
+        );
+    }
+
+    // delete topic test
+    {
+        // arrange
+        let test_topic_name = "test_delete_topic";
+        sut.create_topic(test_topic_name, 1, 1, true)
+            .await
+            .expect("Unable to create a test topic");
+        // act
+        let res = sut.delete_topic(test_topic_name).await;
+        // assert
+        assert!(res.is_ok())
+    }
+
+    // get last offset
+    {
+        // arrange
+        let expected_offset = 13;
+        let test_topic_name = "test_get_last_offset";
+        let producer: FutureProducer = test_fixture.build_kafka_client();
+        sut.create_topic(test_topic_name, 1, 1, true)
+            .await
+            .expect("Unable to create a test topic");
+        // produce some records to increase the offset
+        for i in 0..expected_offset {
+            producer
+                .send(
+                    FutureRecord::to(test_topic_name)
+                        .payload(&format!("test_message {}", i))
+                        .key(&format!("Key {}", i)),
+                    Duration::from_secs(0),
+                )
+                .await
+                .expect("Unable to produce the message");
+        }
+        // act
+        let res = sut
+            .get_last_offsets(&[test_topic_name])
+            .await
+            .expect("get_last_offsets error");
+
+        // assert
+        let topic_result = res.get(test_topic_name).expect("Invalid result");
+        // since there is only one partition we expect to get only one offset
+        assert_eq!(topic_result.len(), 1);
+        assert_eq!(topic_result[0].offset, expected_offset)
+    }
+}
