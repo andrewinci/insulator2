b80c310c2a3efdd0f73142d97588cb2ba4e505a9
andrewinci <andrewinci@gmail.com>
2022-12-29 07:41:50 +0000

    add admin topic integration tests

--- backend/src/integration_tests/admin_it.rs
+++ backend/src/integration_tests/admin_it.rs
@@ -3,6 +3,7 @@ mod integration_tests {
     use rdkafka::{
         config::{ClientConfig, FromClientConfig},
         consumer::{stream_consumer::StreamConsumer, Consumer},
+        producer::{FutureProducer, FutureRecord},
     };
     use std::{collections::HashSet, time::Duration};
     use testcontainers::{clients, images::kafka};
@@ -23,13 +24,13 @@ mod integration_tests {
         kafka_node.start();
 
         let consumer: StreamConsumer = build_kafka_client(&bootstrap_servers.as_str());
-        let cluster_config = build_cluster_config(bootstrap_servers);
+        let cluster_config = build_cluster_config(&bootstrap_servers);
 
         let sut = KafkaAdmin::new(&cluster_config, tmo.clone()).expect("Unable to create the admin client");
-        let test_topic_name = "test_topic_name";
 
         // test create a topic
         {
+            let test_topic_name = "test_topic_name";
             let partition_count = 7_usize;
             // act
             sut.create_topic(test_topic_name, partition_count as i32, 1, false)
@@ -70,6 +71,87 @@ mod integration_tests {
 
             assert_eq!(topic_set, expected_topics)
         }
+
+        // test get topic
+        {
+            // arrange
+            let test_topic_name = "test_get_topic";
+            let partition_count = 3_usize;
+            sut.create_topic(test_topic_name, partition_count as i32, 1, true)
+                .await
+                .expect("Unable to create a test topic");
+            // act
+            let res = sut.get_topic(test_topic_name).expect("Unable to get topic");
+            // assert
+            assert_eq!(res.name, test_topic_name.to_string());
+            assert_eq!(res.partitions.len(), partition_count);
+        }
+
+        // test get topic info
+        {
+            // arrange
+            let test_topic_name = "test_get_topic_info";
+            let partition_count = 3_usize;
+            sut.create_topic(test_topic_name, partition_count as i32, 1, true)
+                .await
+                .expect("Unable to create a test topic");
+            // act
+            let res = sut.get_topic_info(test_topic_name).await.expect("get_topic_info error");
+            // assert
+            assert_eq!(res.name, test_topic_name.to_string());
+            assert_eq!(res.partitions.len(), partition_count);
+            assert_eq!(
+                res.configurations.get("cleanup.policy").unwrap(),
+                &Some("compact".to_string())
+            );
+        }
+
+        // delete topic test
+        {
+            // arrange
+            let test_topic_name = "test_delete_topic";
+            sut.create_topic(test_topic_name, 1, 1, true)
+                .await
+                .expect("Unable to create a test topic");
+            // act
+            let res = sut.delete_topic(test_topic_name).await;
+            // assert
+            assert!(res.is_ok())
+        }
+
+        // get last offset
+        {
+            // arrange
+            let expected_offset = 13;
+            let test_topic_name = "test_get_last_offset";
+            let producer: FutureProducer = build_kafka_client(&bootstrap_servers);
+            sut.create_topic(test_topic_name, 1, 1, true)
+                .await
+                .expect("Unable to create a test topic");
+            // produce some records to increase the offset
+            for i in 0..expected_offset {
+                producer
+                    .send(
+                        FutureRecord::to(test_topic_name)
+                            .payload(&format!("test_message {}", i))
+                            .key(&format!("Key {}", i)),
+                        Duration::from_secs(0),
+                    )
+                    .await
+                    .expect("Unable to produce the message");
+            }
+            // act
+            let res = sut
+                .get_last_offsets(&[test_topic_name])
+                .await
+                .expect("get_last_offsets error");
+
+            // assert
+            let topic_result = res.get(test_topic_name).expect("Invalid result");
+            // since there is only one partition we expect to get only one offset
+            assert_eq!(topic_result.len(), 1);
+            assert_eq!(topic_result[0].offset, expected_offset)
+        }
     }
 
     fn build_kafka_client<T: FromClientConfig>(bootstrap_servers: &str) -> T {
@@ -83,11 +165,11 @@ mod integration_tests {
             .expect("Unable to create the kafka client")
     }
 
-    fn build_cluster_config(bootstrap_servers: String) -> ClusterConfig {
+    fn build_cluster_config(bootstrap_servers: &str) -> ClusterConfig {
         ClusterConfig {
             id: "cluster-id".into(),
             name: "test-cluster-name".into(),
-            endpoint: bootstrap_servers,
+            endpoint: bootstrap_servers.to_string(),
             authentication: crate::lib::configuration::AuthenticationConfig::None,
             schema_registry: None,
             favorites: Favorites::default(),
