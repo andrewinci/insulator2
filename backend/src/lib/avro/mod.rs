dae165519654a5913c24319e99e31c089589e24a
andrewinci <andrewinci@gmail.com>
2023-01-03 12:31:55 +0000

    Add e2e test

--- backend/src/lib/avro/mod.rs
+++ backend/src/lib/avro/mod.rs
@@ -5,6 +5,9 @@ mod helpers;
 mod json_to_avro;
 mod schema_provider;
 
+#[cfg(test)]
+mod parser_e2e_tests;
+
 pub use avro_parser::AvroParser;
 pub use error::AvroError;
 pub use schema_provider::SchemaProvider;
