271367676088add31b90e203c6a15f3038baf981
Andrea <andrewinci@gmail.com>
2023-01-05 07:28:12 +0000

    feat: add avro producer (#307)
    * feat: parse json obj to avro record
    
    * feat: support arrays
    
    * wip: partial union support
    
    * Add support for enum, map and refs
    
    * Add e2e test
    
    * Fix union in avro to json parsing
    
    * Support avro decimal
    
    * Support avro time logical types
    
    * Support avro uuid
    
    * test: add e2e test for refs
    
    * build(deps): bump avro version to use the git master branch
    
    * wip: support references in union
    
    * Use fqdn
    
    * Wrap avro schema into a ref-less struct
    
    * wip: replace avor schema with ref-less schema
    
    * use internal avro schema representation
    
    * cleanup

--- backend/src/lib/avro/error.rs
+++ backend/src/lib/avro/error.rs
@@ -1,16 +1,17 @@
 use crate::lib::schema_registry::SchemaRegistryError;
 
-#[derive(Debug)]
+#[derive(Debug, PartialEq)]
 pub enum AvroError {
     InvalidNumber(String),
-    MissingAvroSchemaReference(String),
     MissingField(String),
     SchemaProvider(String, SchemaRegistryError),
     InvalidUnion(String),
     Unsupported(String),
     InvalidAvroHeader(String),
-    ParseAvroValue(apache_avro::Error),
-    ParseJsonValue(serde_json::Error),
+    ParseAvroValue(String),
+    ParseJsonValue(String),
+    InvalidEnum(String),
+    InvalidUUID(String),
 }
 
 pub type AvroResult<T> = std::result::Result<T, AvroError>;
--- backend/src/lib/schema_registry/mod.rs
+++ backend/src/lib/schema_registry/mod.rs
@@ -1,9 +1,8 @@
 mod client;
 mod error;
 mod http_client;
-mod resolved_avro_schema;
 mod types;
 
 pub use client::CachedSchemaRegistry;
 pub use error::SchemaRegistryError;
-pub use types::{BasicAuth, ResolvedAvroSchema, Schema, Subject};
+pub use types::{BasicAuth, Schema, Subject};
