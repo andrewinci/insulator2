0bada9ffb19bc43c373cde57aeff8221af13c866
andrewinci <andrewinci@gmail.com>
2023-02-05 06:45:56 +0000

    chore: add parameter to show notifications in modals

--- frontend/src/helpers/notification.tsx
+++ frontend/src/helpers/notification.tsx
@@ -1,13 +1,18 @@
 import { IconCheck, IconX } from "@tabler/icons";
 import { showNotification } from "@mantine/notifications";
+import { isRunningInModal } from "../hooks";
 
 export type Notification = {
   type: "ok" | "error";
   title?: string;
   description?: string;
+  showInModal?: boolean;
 };
 
 export const addNotification = (n: Notification) => {
+  // Don't show notifications in modal if they are not explicitly allowed
+  // or if they are not errors
+  if (isRunningInModal() && !n.showInModal && n.type !== "error") return;
   showNotification({
     id: n.description,
     autoClose: n.type === "ok" ? 3000 : false,
@@ -18,18 +23,20 @@ export const addNotification = (n: Notification) => {
   });
 };
 
-export const notifySuccess = (title: string, description?: string) => {
+export const notifySuccess = (title: string, description?: string, showInModal?: boolean) => {
   addNotification({
     type: "ok",
     title,
     description,
+    showInModal,
   });
 };
 
-export const notifyFailure = (title: string, description?: string) => {
+export const notifyFailure = (title: string, description?: string, showInModal?: boolean) => {
   addNotification({
     type: "error",
     title,
     description,
+    showInModal,
   });
 };
--- frontend/src/hooks/use-parsed-url.ts
+++ frontend/src/hooks/use-parsed-url.ts
@@ -1,17 +1,21 @@
 import { matchPath, useLocation } from "react-router-dom";
 import { useUserSettings } from "../providers";
 
+export const isRunningInModal = () => {
+  const { base } = matchPath("/:base/*", location.pathname)?.params ?? {};
+  return base === "modal";
+};
+
 /// get the cluster name from the cluster id in the url
 export const useParsedUrl = () => {
   const location = useLocation();
   const { userSettings: appState } = useUserSettings();
-  const { base } = matchPath("/:base/*", location.pathname)?.params ?? {};
-  const pathPattern =
-    base === "modal" ? "/modal/cluster/:clusterId/:activeItem/*" : "/cluster/:clusterId/:activeItem/*";
+  const isModal = isRunningInModal();
+  const pathPattern = isModal ? "/modal/cluster/:clusterId/:activeItem/*" : "/cluster/:clusterId/:activeItem/*";
   const { clusterId, activeItem } = matchPath(pathPattern, location.pathname)?.params ?? {};
   const clusterName = appState.clusters.find((c) => c.id == clusterId)?.name;
   return {
-    isModal: base === "modal",
+    isModal,
     clusterName,
     clusterId,
     activeItem,
